433f18a0cd8792dd2c2d350336b390c538c97571
Lijcoesh <lucanoort@gmail.com>
2025-11-29 12:45:32 +0100

    fix footer imports

--- src/pages/Homepage/Footer.tsx
+++ src/pages/Homepage/Footer.tsx
@@ -1,6 +1,6 @@
 import { Box, Container, Typography } from "@mui/material";
-import { BodyText } from "../Landingpage/Components/ContextBox/Components/Text/BodyText";
-import { PixelButton } from "../../../Components/Buttons/PixelButton";
+import { BodyText } from "../../Components/Text/BodyText";
+import { PixelButton } from "../../Components/Buttons/PixelButton";
 
 export default function Footer() {
   return (
