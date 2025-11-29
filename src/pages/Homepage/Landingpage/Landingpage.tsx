fcb811f4049473e7d3b7169dc8cee0f63fa8cecc
Lijcoesh <lucanoort@gmail.com>
2025-11-29 12:25:28 +0100

    remove box

--- src/pages/Homepage/Landingpage/Landingpage.tsx
+++ src/pages/Homepage/Landingpage/Landingpage.tsx
@@ -1,4 +1,4 @@
-import { Stack, Box } from "@mui/material";
+import { Stack } from "@mui/material";
 import { ChevronLeft, ChevronRight } from "./Components/Chevron";
 import { LogoText } from "./Components/ContextBox/Components/Text/LogoText";
 import ContextBox from "./Components/ContextBox/ContextBox";
