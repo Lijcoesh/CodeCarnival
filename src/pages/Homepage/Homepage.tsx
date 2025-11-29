ffc80372dca4553fed9593bbb8055608b9b68844
Lijcoesh <lucanoort@gmail.com>
2025-11-29 13:28:51 +0100

    fix bug

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,12 +1,10 @@
 import Landingpage from "./Landingpage";
 import CodeFeatures from "./CodeFeatures";
 import Footer from "./Footer";
-import AccountButton from "../../Components/Buttons/AccountButton";
 
 export default function Homepage() {
   return (
     <>
-      <AccountButton />
       <Landingpage />
       <CodeFeatures />
       <Footer />
