b3cc866ac1884f090296ab384cb285fdfdfe58a9
Lijcoesh <lucanoort@gmail.com>
2025-11-29 12:30:47 +0100

    remove theme

--- src/theme/typography.ts
+++
@@ -1,3 +0,0 @@
-export const typography = {
-  fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
-};
